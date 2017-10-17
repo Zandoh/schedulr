ac28f6848b7b3410a49087388c07ee642413a634
Mockery <kbm5683@rit.edu>
2017-10-17 14:04:50 -0400

    Updated playground to be more compact.

--- playground.php
+++ playground.php
@@ -4,43 +4,12 @@
 
 <html>
 <head>
-  <title></title>
-  <link rel="stylesheet" href= "assets/css/vendor/bootstrap.min.css" type="text/css">
-  <link rel="stylesheet" href= "assets/fullcalendar/fullcalendar.min.css" type="text/css">
-  <link rel="stylesheet" href= "assets/fullcalendar/fullcalendar.print.css" type="text/css">
-  <link rel="stylesheet" href= "assets/css/styles.css" type="text/css">
-</head>
-<!-- <nav class="navbar navbar-inverse fixed-top bg-primary ">
-  <a class="navbar-brand" href="#">RAIHN Management Tool</a>
-</nav> -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="">
-    <img src="assets/img/raihn-logo.png" width="30" height="30" class="d-inline-block align-top" alt="RAIHN Logo">
-      RAIHN Management Tool
-  </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+  
+<?php
+include 'assets/includes/header.php';
+include 'assets/includes/nav.php';
+?>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home</a>
-      </li>
-    </ul>
-    <ul class="navbar-nav pull-right">
-      <li class="nav-item">
-        <a class="nav-link" href="#">View</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Manage</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Logout</a>
-      </li>
-    </ul>
-  </div>
-</nav>
 <body>
 
 <div id="calendar"></div>
