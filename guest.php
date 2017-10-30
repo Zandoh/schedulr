6c42e99c8d4ea27f9bf43c10d0af9589e0d9312c
Amber Normand <amber.normand7@gmail.com>
2017-10-29 20:18:52 -0400

    test

--- guest.php
+++ guest.php
@@ -19,17 +19,19 @@
 		//check if a schedule has been selected to view
 		if (isset($_POST['congregationScheduleForm'])){
 			//if a schedule has been selected from the select then display the congregations as a table
-			echo createSchedule(''.$_POST['congregationScheduleList'].'');
+			echo createSchedule($_POST['congregationScheduleList']);
 		}
 
 
-        include 'assets/includes/footer.php';
-    ?>
-    </body>
+	include 'assets/includes/footer.php';
+	?>
+
+	</body>
+
     <script src="assets/js/vendor/jquery-3.2.1.min.js" type="text/javascript"></script>
-    <script src="assets/js/vendor/jquery-ui.min.js" type="text/javascript"></script>
-    <script src="assets/js/vendor/jquery-ui.multidatespicker.js" text="text/javascript"></script>
     <script src="assets/js/vendor/popper.js" type="text/javascript"></script>
     <script src="assets/js/vendor/bootstrap.min.js" type="text/javascript"></script>
+    <script src="assets/fullcalendar/lib/moment.min.js" type="text/javascript"></script>
+    <script src="assets/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
     <script src="assets/js/scripts.min.js" type="text/javascript"></script>
 </html>
