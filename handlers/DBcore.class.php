9f3459ca9749cfee9c23fbef7d562af33bf99eee
Amber Normand <amber.normand7@gmail.com>
2017-11-02 10:52:33 -0400

    DB calls
    for busdriver and congregations

--- handlers/DBcore.class.php
+++ handlers/DBcore.class.php
@@ -56,6 +56,61 @@ class DBcore{
 		return $data;	
 	}//end of select all Congregation Schedule Events
     
+    
+	/*
+	* Select all Bus Drivers
+	*/
+	function selectAllBusDrivers(){
+		$data = array();
+		if($stmt = $this->conn->prepare("select user_ID, first_name, last_name from USER WHERE user_type='B';")){
+			$stmt->execute();
+			$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+		return $data;	
+	}
+
+
+	/*
+	* Select a Bus Drivers availability
+	*/
+	function selectBusDriverAvailability($user_ID){
+		$data = array();
+		if($stmt = $this->conn->prepare("select ad.availability_day, ad.availabilty_time_of_day from AVAILABILTY_DATE ad JOIN BUS_DRIVER_AVAILABILTY bda using(availability_date_ID) WHERE bda.user_ID=:user_ID;")){
+            $stmt->bindParam(':user_ID', $user_ID);
+			$stmt->execute();
+			$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+		return $data;	
+	}
+
+	/*
+	* Select all congregations
+	*/
+	function selectAllCongregations(){
+		$data = array();
+		if($stmt = $this->conn->prepare("select congregation_ID, congregation_name from CONGREGATION;")){
+			$stmt->execute();
+			$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+		return $data;	
+	}//end of select all Congregations
+
+
+	/*
+	* Select a Congregations blackout dates
+	*/
+	function selectCongregationBlackoutDates($congregation_ID){
+		$data = array();
+		if($stmt = $this->conn->prepare("select bd.blackout_date_start, bd.blackout_date_end from BLACKOUT_DATE bd JOIN CONGREGATION_BLACKOUT_DATE cbd using(blackout_date_ID) WHERE cbd.congregation_ID=:congregation_ID;")){
+            $stmt->bindParam(':congregation_ID', $congregation_ID);
+			$stmt->execute();
+			$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		}
+		return $data;	
+	}
+
+
+
     /*
 	* Validate user login
 	*/
