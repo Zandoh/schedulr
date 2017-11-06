3c59e44c98b19b542d01b5f767b82e3fd5208271
Amber Normand <amber.normand7@gmail.com>
2017-11-05 21:08:02 -0500

    create logout page

--- bus-drivers.php
+++ bus-drivers.php
@@ -21,7 +21,7 @@
             <a class="nav-link" href="login_landing.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="login.php">Logout</a>
+            <a class="nav-link" href="logout.php">Logout</a>
           </li>
         </ul>
       </div>
--- login_landing.php
+++ login_landing.php
@@ -6,6 +6,16 @@
     //Check if the SESSION is already set
     if (isset($_SESSION['userLogin'])) {
         // logged in
+        //user is a bus driver
+        if(strcasecmp($_SESSION[$userType],"b") == 0){
+          //redirect the busdriver to put in availability
+          header("Location: bus-avail.php");
+        }
+        //user is a congregation lead
+        elseif(strcasecmp($_SESSION[$userType],"c") == 0){
+          //redirect the congregation lead to put in blackout dates
+          header("Location: cong-avail.php");
+        }
     }
     else{
         //not logged in
@@ -28,7 +38,7 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link" href="/login.php">Logout</a>
+            <a class="nav-link" href="logout.php">Logout</a>
           </li>
         </ul>
       </div>
