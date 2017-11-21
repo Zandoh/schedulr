e3dd2467c2101c666b6ac0d8dc8a93eb631fafb7
Amber Normand <amber.normand7@gmail.com>
2017-11-21 15:57:58 -0500

    updated url of the logo

--- assets/includes/nav.php
+++ assets/includes/nav.php
@@ -6,7 +6,7 @@
     } 
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark">
-  <a class="navbar-brand" href="/schedulr">
+  <a class="navbar-brand" href="/login.php">
     <img src="assets/img/raihn-logo.png" width="187" height="60" class="d-inline-block" alt="RAIHN Logo">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
