5aab7e43b5cc2871aadc795d7c72ab1471bebaf9
men6288 <men6288@rit.edu>
2017-11-28 14:32:56 -0500

    Added sessions and stuff

--- blackouts.php
+++ blackouts.php
@@ -1,6 +1,22 @@
 <html>
 
 <?php
+    // begin session
+    session_start(); 
+    // Check if the SESSION is already set
+   if (!isset($_SESSION['userLogin'])) {
+        // if the user is not logged in, redirect them to the login page
+        header("Location:login.php");
+    }
+    // logged in, check which page to redirect to
+    elseif(strcasecmp($_SESSION['userType'],"b") == 0){
+      header("Location: bus-avail.php");
+    }
+    // user is a congregation lead
+    elseif(strcasecmp($_SESSION['userType'],"c") == 0){
+      header("Location: blackouts.php");
+    }
+    
     include 'handlers/login_handler.php';
     include 'assets/includes/header.php';
     ?>
