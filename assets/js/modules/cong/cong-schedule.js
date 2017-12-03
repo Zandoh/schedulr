e6dc5ad9fccf680efab3d9175f9f3668547998ce
Kyle Mochrie <kbm5683@rit.edu>
2017-12-03 16:00:53 -0500

    Front-end congregation schedule update to switch around the dates.

--- assets/js/modules/cong/cong-schedule.js
+++ assets/js/modules/cong/cong-schedule.js
@@ -15,11 +15,30 @@ var cong_schedule = {
   * Usage: Called when congregation schedule is initialized
   */
   bindEvents: function() {
-    $(function() {
-      $("table.sortCongregations").sortable({
-        items: "td.sort-cong-name"
-      }).disableSelection();
-    });
+    makeDraggable();
+    function makeDraggable() {
+        $(".sort-cong-name").draggable({
+            helper: "clone",
+            start: function(e) {
+              placeholder = e.target;
+              $(placeholder).addClass("opacity");
+            },
+            stop: function(e){
+              $(placeholder).removeClass("opacity");
+              placeholder = null;
+            }  
+            
+        });
+        $(".sort-cong-name").droppable({
+            drop: function (e, ui) {
+                $(placeholder).removeClass("opacity");
+                placeholder = null;
+                $(ui.draggable).clone().replaceAll(this);
+                $(this).replaceAll(ui.draggable);
+                makeDraggable();
+            },
+        });
+    }
   }
 
 }
